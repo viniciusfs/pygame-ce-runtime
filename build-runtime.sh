50dceba2e9dd2fa305bde997e936e8786bec2029
Vinicius Figueiredo <vinicius.figueiredo@bemobi.com>
2025-11-09 19:13:01 -0300

    (feat): improve build script

--- build-runtime.sh
+++ build-runtime.sh
@@ -1,23 +1,44 @@
 #!/usr/bin/env bash
 
+: "${PYTHON_VERSION:?PYTHON_VERSION is required}"
+: "${PYGAME_VERSION:?PYGAME_VERSION is required}"
+
+export PY_VER=${PYTHON_VERSION%.*}
+
 cd $HOME
 
+# install dependencies
 apt update && apt install -y build-essential git rsync squashfs-tools \
   libfreetype6-dev libportmidi-dev python3-dev python3-numpy \
   libsdl2-dev libsdl2-image-dev libsdl2-mixer-dev libsdl2-ttf-dev
 
-python -m venv /runtime
-source /runtime/bin/activate
+# run pre-install script
+if [[ -f /local/build/pre-install.sh ]]; then
+  bash /local/build/pre-install.sh
+fi
 
-git clone --branch 2.5.6 https://github.com/pygame-community/pygame-ce.git
+# create runtime
+python -m venv --copies /runtime
+source /runtime/bin/activate
+git clone --branch ${PYGAME_VERSION} https://github.com/pygame-community/pygame-ce.git
 cd pygame-ce && python -m pip install .
 
-rm /runtime/bin/python
-cp /usr/local/bin/python /runtime/bin/python
-cp /usr/local/lib/libpython3.12.so.1.0 /runtime/lib/
-rsync -av /usr/local/lib/python3.12/ /runtime/lib/python3.12/
-find /runtime -name "__pycache__" -type d -exec rm -rf {} +
+# install additional requirements
+if [[ -f /local/build/requirements.txt ]]; then
+  python -m pip install -r /local/build/requirements.txt
+fi
 
 deactivate
 
-mksquashfs /runtime /local/build/pygame-ce_2.5.6_python_3.12.8.squashfs -comp xz -b 1M
+# copy python library
+cp /usr/local/lib/libpython${PY_VER}.so.1.0 /runtime/lib/
+rsync -av /usr/local/lib/python${PY_VER}/ /runtime/lib/python${PY_VER}/
+find /runtime -name "__pycache__" -type d -exec rm -rf {} +
+
+# run post-install script
+if [[ -f /local/build/post-install.sh ]]; then
+  bash /local/build/post-install.sh
+fi
+
+# create squashfs from runtime
+mksquashfs /runtime /local/build/pygame-ce_${PYGAME_VERSION}_python_${PYTHON_VERSION}.squashfs -comp xz -b 1M
